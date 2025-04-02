9958b254e295299a3f2c22edc8a86c72e873c719
henriqueoliveira03 <luizhomartins2@gmail.com>
2025-04-02 19:19:52 -0300

    Add files via upload

---
+++ ClassLibrary1/Membros.cs
@@ -0,0 +1,13 @@
+﻿namespace Jeruel.Teste
+
+{
+    public class Membros
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string CPF { get; set; }
+        public DateTime DataDeNascimento { get; set; }
+    }
+}
